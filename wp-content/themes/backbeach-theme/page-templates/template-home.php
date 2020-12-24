6e269228ed196e0e6e375d08a227411d410001af
Ericcentrik <enaiman1@gmail.com>
2020-12-24 12:24:58 -0500

    finished  about section

--- wp-content/themes/backbeach-theme/page-templates/template-home.php
+++ wp-content/themes/backbeach-theme/page-templates/template-home.php
@@ -73,7 +73,7 @@ get_header();
     </div>
 </div>
 
-    <div class="home-news">
+<div class="home-news">
         <div class="bg-image"></div>
         <div class="container">
             <div class="row align-items-center">
@@ -98,34 +98,46 @@ get_header();
 
         </div>
     </div>
+</div>
 
-    <div class="home-about">
-    <div class="bg-image"></div>
-    <div class="title-holder">
-        <div class="container">
-            <h2><span>Welcome To</span> The BackBeach Eating House</h2>
+<div class="home-about">
+    <div class="bg-image">
+        <!-- top cutout -->
+             <div class="top-overlay">
+               <img src="<?php echo get_template_directory_uri(); ?>/img/about-bottom.png" alt="Cutout Top" width="100%">
+        </div>
+        <!-- bottom cutout -->
+        <div class="bottom-overlay">
+            <img src="<?php echo get_template_directory_uri(); ?>/img/about-top.png" alt="Cutout Bottom" width="100%">
+        </div>
+        
+    </div>
+        <div class="title-holder">
+         <div class="container">
+                <h2><span>Served Fresh</span> From the paddock to the plate</h2>
             </div>
             </div>
-        <div class="container">
-      <div class="row">
-          <div class="col-lg-1"></div>
-          <div class="col-lg-8">
-          <p class="tagline">
-        There's only one place like the Backbeach Cafe & Restaurant, located on Bunbury's Baclbeach with uninterrupted views of crip white sand and the Indian Ocean. The perfect place to eat, relax and enjoy.
-        </p>
-        <p>
-        We are proud to support local business and local producers by sourcing as much as possible locally, including fish from Augusta, beef and chicken from Western Australia (DBC), coffee from Yallingup and wines from Ferguson Valley, Margaret River and throughout the South West.
-        </p>
-          </div>
-          <div class="col-lg-3">
-              <div class="button">
+            <div class="container">
+            <div class="row">
+                <div class="col-lg-1"></div>
+                <div class="col-lg-8">
+                    <p class="tagline">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nisl eros, 
+                        pulvinar facilisis justo mollis, auctor consequat urna
+                    </p>
+                    <p>
+                        Morbi a bibendum metus.Donec scelerisque sollicitudin enim eu venenatis. Duis tincidunt laoreet ex, in pretium orci vestibulum eget. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+                    </p>
+                </div>
+                <div class="col-lg-3">
+                <div class="button">
                   Read More <i class="fa fa-chevron-right"></i>
                 </div>
           </div>
-      </div>
-        </div>
+       </div>
     </div>
 </div>
+
 
 <?php
 get_footer();
